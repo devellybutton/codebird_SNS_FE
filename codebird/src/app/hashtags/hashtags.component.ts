d440c177977ba29012ab4e446ca6b77d19971bb7
devellybutton <leegr0106@gmail.com>
2025-01-23 00:57:01 +0900

    codebird 프로젝트 초기 세팅

---
+++ codebird/src/app/hashtags/hashtags.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hashtags',
+  imports: [],
+  templateUrl: './hashtags.component.html',
+  styleUrl: './hashtags.component.scss'
+})
+export class HashtagsComponent {
+
+}
